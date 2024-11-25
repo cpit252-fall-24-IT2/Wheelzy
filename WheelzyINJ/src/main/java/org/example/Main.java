59f18d1c33d76f2a263c5b1710ddd3c96b13041c
Abdulrahman Alsyami <Ab.alsyami@users.noreply.github.com>
2024-11-25 22:00:30 +0300

    adding array for cars

--- WheelzyINJ/src/main/java/org/example/Main.java
+++ WheelzyINJ/src/main/java/org/example/Main.java
@@ -1,13 +1,17 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 // our main class :D
-public class Main
-{
-    public static void main( String[] args )
-    {
+public class Main {
+    private static AuthSystem authSystem = new AuthSystem();
+    private static List<Car> cars = new ArrayList<>();
+
+    public static void main(String[] args) {
+
         Car car1 = CarFactory.createCar("Sedan", "2022", "Toyota - Prado", 400, "Standard", 0);
         car1.displayCarInfo();
-
 
 
     }
