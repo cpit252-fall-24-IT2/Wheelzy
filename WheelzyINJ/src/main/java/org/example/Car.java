b3ba71fd562d19dfc4fbd9e82fc664eb233be14e
Abdulrahman Alsyami <Ab.alsyami@users.noreply.github.com>
2024-11-25 21:47:59 +0300

    adding builder(not full)

--- WheelzyINJ/src/main/java/org/example/Car.java
+++ WheelzyINJ/src/main/java/org/example/Car.java
@@ -1,15 +1,45 @@
 package org.example;
-// the abstract class for SUV and Sedan
-public abstract class Car {
-    protected String make;
-    protected String model;
-    protected double price;
 
-    protected Car(String make, String model, double price) {
-        this.make = make;
-        this.model = model;
-        this.price = price;
+// the abstract class for SUV and Sedan
+public class Car {
+    private final String make;
+    private final String model;
+    private final double price;
+
+    private Car(Builder builder) {
+        this.make = builder.setMake();
+        this.model = builder.setModel();
+        this.price = builder.setPrice();
     }
 
-    public abstract void displayCarInfo();
+    public void displayCarInfo() {
+
+    }
+
+
+    public class Builder {
+        private String make;
+        private String model;
+        private double price;
+
+        public Builder setMake(String make) {
+            this.make = make;
+            return this;
+        }
+
+        public Builder setModel(String model) {
+            this.model = model;
+            return this;
+        }
+
+        public Builder setPrice(double price) {
+            this.price = price;
+            return this;
+        }
+
+
+        public Car build() {
+            return new Car(this);
+        }
+    }
 }
