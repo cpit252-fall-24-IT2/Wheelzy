81a1433f6bb42307b75be23472b79f5bfdef0c7c
Abdulrahman Alsyami <Ab.alsyami@users.noreply.github.com>
2024-12-01 20:52:34 +0300

    fix main

--- WheelzyINJ/src/main/java/org/example/Main.java
+++ WheelzyINJ/src/main/java/org/example/Main.java
@@ -5,17 +5,12 @@ import java.util.List;
 
 // our main class :D
 public class Main {
-    private static AuthSystem authSystem = new AuthSystem();
-    private static List<Car> cars = new ArrayList<>();
 
     public static void main(String[] args) {
         CarRentalSystem rentalSystem = CarRentalSystem.getInstance();
         rentalSystem.start();
         rentalSystem.initialize();
-        // rentalSystem.processUserInput();
-
-        Car car1 = CarFactory.createCar("Sedan", "2022", "Toyota - Prado", 400, "Standard", 0);
-        car1.displayCarInfo();
+        rentalSystem.processUserInput();
 
 
 
